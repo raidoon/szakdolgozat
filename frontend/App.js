fe2662f8788fd40e2c566057d257c16ac6838290
raidoon <thomaselphin@gmail.com>
2024-11-18 08:47:22 +0100

    updating folder structure

--- frontend/App.js
+++ frontend/App.js
@@ -8,12 +8,52 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
 import Ionicons from "react-native-vector-icons/Ionicons";
 //--------------------------------------------------- oldalak importálása
-import Kezdolap from "./screens/Kezdolap";
-import Profil from "./screens/Profile";
-import Datumok from "./screens/Datumok";
-import Befizetesek from "./screens/Befizetesek";
+import Kezdolap from "./screens/App/Kezdolap";
+import Befizetesek from "./screens/App/Befizetesek";
+import Datumok from "./screens/App/Datumok";
+import Profil from "./screens/App/Profile";
 //--------------------------------------------------- kinézet import
 import Styles from './Styles';
+
+
+//--------------------------------------------------- ide jön a connection
+
+/*
+
+PÉLDA:
+
+--------> Your Parse initialization configuration goes here
+
+Parse.setAsyncStorage(AsyncStorage);
+
+const PARSE_APPLICATION_ID = 'YOUR_PARSE_APPLICATION_ID';
+const PARSE_HOST_URL = 'https://parseapi.back4app.com/';
+const PARSE_JAVASCRIPT_ID = 'YOUR_PARSE_JAVASCRIPT_ID';
+Parse.initialize(PARSE_APPLICATION_ID, PARSE_JAVASCRIPT_ID);
+Parse.serverURL = PARSE_HOST_URL; 
+
+------------> KOYEB Node.JS: 
+
+.ENV
+
+DATABASE_HOST="<DB_HOSTNAME>"
+DATABASE_USER="<DB_ROLE>"
+DATABASE_PASSWORD="<DB_PASSWORD>"
+DATABASE_NAME="<DB_DATABASE_NAME>"
+
+INDEX.TS
+
+import postgres from 'postgres'
+ 
+const sql = postgres({
+  host: process.env.DATABASE_HOST,
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
+  ssl: 'require',
+})
+*/
+
 //---------------------------------------------------  navigátorok
 const Drawer = createDrawerNavigator();
 const Stack = createStackNavigator();
@@ -44,6 +84,13 @@ function Regisztracio(){
   );
 }
 //--------------------------------------------------- fő app
+
+
+//IDE MÉG NEM A TAB NAVIGÁTOR KELL, HANEM EGY STACK NAVIGÁTOR, AMIBEN BENNE VAN A BEJELENTKEZÉS, A REGISZTRÁCIÓ ÉS AZ ALAP HOME OLDAL, AMIBEN A FELHASZNÁLÓ KI TUDJA VÁLASZTANI, HOGY BEJELENTKEZNI AKAR VAGY REGISZTRÁLNI !!!
+
+// A TAB NAVIGÁTOR LEGYEN EGY KÜLÖN FUNCTION, AMI NEM AKKOR JÖN BE, AMIKOR BELÉPÜNK AZ APPBA, HANEM AKKOR, HA A BEJELENTKEZÉS SIKERES VOLT
+
+
 export default function App() {
   return(
     <NavigationContainer>
