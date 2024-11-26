d1b2cd5389f3d5e6ddf1139d48109923f6d67145
slightlyfanni <bodnarfanni2003@gmail.com>
2024-11-26 14:23:12 +0100

    befizetes_levetel

--- backend/gyakorlo_backend.js
+++ backend/gyakorlo_backend.js
@@ -28,19 +28,17 @@ app.post('/bejelentkezes', (req, res) => {
   connection.query(`
   SELECT tanulo.tanulo_nev, tanulo.tanulo_oktatoja, tanulo.tanulo_orak, 
        tanulo.tanulo_felhasznaloID, felhasznalo.felhasznalo_username, 
-       felhasznalo.felhasznalo_jelszo, fizetes.befizetett_osszeg, 
-       fizetes.befizetes_ideje, oktato.oktato_nev, autosiskola.autosiskola_neve
-      FROM tanulo_adatok AS tanulo
-      INNER JOIN felhasznaloi_adatok AS felhasznalo 
-      ON tanulo.tanulo_felhasznaloID = felhasznalo.felhasznalo_id
-      INNER JOIN befizetesek AS fizetes 
-      ON tanulo.tanulo_id = fizetes.tanulo_id
-      INNER JOIN oktato_adatok AS oktato 
-      ON tanulo.tanulo_oktatoja = oktato.oktato_id
-      INNER JOIN autosiskola_adatok AS autosiskola 
-      ON oktato.autosiskola_id = autosiskola.autosiskola_id
-      WHERE felhasznalo.felhasznalo_username = ? 
-      AND felhasznalo.felhasznalo_jelszo = ? `,[req.body.bevitel1, req.body.bevitel2], (err, rows, fields) => {
+       felhasznalo.felhasznalo_jelszo, oktato.oktato_nev, autosiskola.autosiskola_neve
+FROM tanulo_adatok AS tanulo
+INNER JOIN felhasznaloi_adatok AS felhasznalo 
+    ON tanulo.tanulo_felhasznaloID = felhasznalo.felhasznalo_id
+    INNER JOIN oktato_adatok AS oktato 
+    ON tanulo.tanulo_oktatoja = oktato.oktato_id
+    INNER JOIN autosiskola_adatok AS autosiskola 
+    ON oktato.autosiskola_id = autosiskola.autosiskola_id
+  WHERE felhasznalo.felhasznalo_username = ? 
+    AND felhasznalo.felhasznalo_jelszo = ?
+  `,[req.body.bevitel1, req.body.bevitel2], (err, rows, fields) => {
     if (err) {
       console.log("Hiba")
       res.status(500).send("Hiba")
