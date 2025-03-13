5c6bd621a493244950fda864fe31637b2423fbb4
raidoon <thomaselphin@gmail.com>
2025-03-13 13:39:54 +0100

    uploading backend to server

--- frontend_web/src/Ipcim.js
+++ frontend_web/src/Ipcim.js
@@ -1,8 +1,3 @@
 module.exports = {
-    Ipcim: 'http://localhost:3000',
-    //Ipcim: 'http://192.168.10.57:3001',
-    //Ipcim: 'http://192.168.10.58:3001',
-    //Ipcim: 'http://192.168.1.190:3001',
-    //Ipcim: 'http://192.168.1.234:3001',
-    //Ipcim: 'http://192.168.0.104:3001'
+    Ipcim: 'https://nodejs319.dszcbaross.edu.hu',
 };
